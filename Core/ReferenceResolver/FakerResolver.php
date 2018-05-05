45edc55e0527050f5d7df19be509cd0dee67ade7
Gaetano Giunta <ggiunta@kaliop.com>
2018-05-05 15:51:14 +0100

    add faker resolver

---
+++ Core/ReferenceResolver/FakerResolver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Kaliop\eZLoremIpsumBundle\Core\ReferenceResolver;
+
+use Faker\Factory;
+use Kaliop\eZMigrationBundle\Core\ReferenceResolver\AbstractResolver;
+use Kaliop\eZMigrationBundle\API\EnumerableReferenceResolverInterface;
+
+class FakerResolver extends AbstractResolver implements EnumerableReferenceResolverInterface
+{
+    protected $referencePrefixes = array('faker:');
+
+    protected $faker;
+
+    public function __construct($locale = null)
+    {
+        parent::__construct();
+
+        if ($locale === null) {
+            /// @todo get default locale from current siteaccess
+            $locale = 'eng-GB';
+        }
+        $locale = $this->convertLocale($locale);
+        $this->faker = Factory::create($locale);
+    }
+
+    /**
+     * @param string $identifier format: 'faker:name', 'faker:randomnumber(3)'
+     * @return mixed
+     * @throws \Exception When trying to retrieve an unset reference
+     */
+    public function getReferenceValue($identifier)
+    {
+        $identifier = $this->getReferenceIdentifier($identifier);
+
+        /// @todo this way of matching args does not allow for arguments which contain a comma or right parethesis chars...
+        if (preg_match('/^([^(]+)\(([^(]+)\)$/', $identifier, $matches)) {
+            $formatter = $matches[1];
+            $arguments = array_map('trim', explode(',', $matches[2]));
+        } else {
+            $formatter = $identifier;
+            $arguments = array();
+        }
+
+        return $this->faker->format($formatter, $arguments);
+    }
+
+    /**
+     * converts eng-GB in en_GB
+     * @param $locale string
+     * @return string
+     */
+    protected function convertLocale($locale)
+    {
+        return substr($locale, 0, 2) . '_' . substr($locale, 4);
+    }
+
+    /**
+     * We implement this method (interface) purely for convenience, as it allows this resolver to be added to the
+     * 'custom-reference-resolver' chain and not break migration suspend/resume
+     * @return array
+     */
+    public function listReferences()
+    {
+        return array();
+    }
+}
--- README.md
+++ README.md
@@ -4,11 +4,56 @@ ezloreimpsumbundle
 A bundle for eZPublish5 / eZPlatform dedicated to mass-creation of sample/test content.
 
 
-## How it works, in 10 lines of yaml
+## How it works, in 30 lines of yaml
 
-        to be documented... 
+        -
+            type: loop
+            repeat: 5
+            steps:
+                -
+                    type: content
+                    mode: create
+                    content_type: folder
+                    parent_location: 2
+                    attributes:
+                        name: "Folder [loop:iteration] - [faker:sentence(3)]"
+                    references:
+                        -
+                            attribute: location_id
+                            identifier: parent_folder_loc
+                            overwrite: true
+                -
+                    type: loop
+                    repeat: 3
+                    steps:
+                        -
+                            type: content
+                            mode: create
+                            content_type: folder
+                            parent_location: reference:parent_folder_loc
+                            attributes:
+                                name: "Folder [faker:unixTime]"
+ 
 
 ## Requirements
 
 * eZPublish 2014.11 or later or eZPlatform
 * eZMigrationBundle
+* Faker from FZaninotto
+
+## Installation
+
+1. get the bundle via composer
+
+    ```
+    composer require kaliop/ezloreipsumbundle
+    ```
+
+2. activate it in your Kernel's `registerBundles()` method
+
+    ```
+    public function registerBundles()
+    {
+        ...
+        new \Kaliop\eZLoremIpsumBundle\EzLoremIpsumBundle()
+    ```
